1f95a8b0eb60fbc0c475145a51a24912ec291914
Soikat <193371447+soikat27@users.noreply.github.com>
2026-07-23 18:06:15 -0400

    Implement player class module
    -- two types of players: 1. "real" 2. computer

---
+++ src/modules/player.js
@@ -0,0 +1,7 @@
+export default class Player {
+    constructor(type, gameboard, name=undefined) {
+        this.type = type;
+        this.gameboard = gameboard;
+        this.name = name;
+    }
+}
